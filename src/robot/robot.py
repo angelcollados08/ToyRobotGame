ab3e4b7c2f5a758c01bec18662e369a01f68ad60
angelcollados08 <angelcollados08@gmail.com>
2025-10-02 22:32:38 +0200

    Add robot attributes and functions to report the position or set the position of the robot

---
+++ src/robot/robot.py
@@ -0,0 +1,16 @@
+from dataclasses import dataclass
+from position import Position
+from direction import Direction
+
+@dataclass
+class Robot():
+    position: Position
+    facing: Direction
+
+    def set_robot(self,pos: Position, dir: Direction) -> None:
+        self.position = pos
+        self.facing = dir
+
+    def report(self) -> str:
+        return f"{self.position.x +1},{self.position.y + 1},{self.facing}"
+
