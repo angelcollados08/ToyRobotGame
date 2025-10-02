cd0fb7f34c70eac62968d9ac6d99b662d9006666
angelcollados08 <angelcollados08@gmail.com>
2025-10-02 23:05:34 +0200

    Add mechanism to rotate the robot direction

--- src/direction/direction.py
+++ src/direction/direction.py
@@ -5,3 +5,11 @@ class Direction(Enum):
     EAST = (1,0)
     SOUTH = (0,-1)
     WEST = (-1,0)
+
+    def turn(self,turn:str):
+        directions = list(Direction)
+        if turn == "LEFT":
+            new_dir = (directions.index(self)-1) % len(directions)
+        elif turn == "RIGHT":
+            new_dir = (directions.index(self) + 1) % len(directions)
+        return directions[new_dir]
